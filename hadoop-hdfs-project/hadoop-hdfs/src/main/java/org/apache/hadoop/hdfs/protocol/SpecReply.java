480c853ca38e7f88f3246305539efa8b3a63d52f
Lixiang Ao <liao@eng.ucsd.edu>
2017-05-22 17:49:08 -0700

    request reply

---
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocol/SpecReply.java
@@ -0,0 +1,7 @@
+package org.apache.hadoop.hdfs.protocol;
+
+/**
+ * Created by aolx on 5/22/17.
+ */
+public class SpecReply {
+}
---
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocol/SpecRequest.java
@@ -0,0 +1,12 @@
+package org.apache.hadoop.hdfs.protocol;
+
+/**
+ * Created by aolx on 5/22/17.
+ */
+public class SpecRequest {
+    private String op;
+
+    public SpecRequest(String str) {
+
+    }
+}
