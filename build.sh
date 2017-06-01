142725328187cd12ad34871899911026e8da67d6
Lixiang Ao <liao@eng.ucsd.edu>
2017-06-01 16:18:58 -0700

    one-button build with build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd hadoop-hdfs-project/hadoop-hdfs/src/main/specpaxos
+make finalLib -j4
+cd -
+mvn package -Pdist -DskipTests -Dmaven.javadoc.skip=true -Dtar
--- hadoop-common-project/hadoop-common/src/main/conf/hadoop-env.sh
+++ hadoop-common-project/hadoop-common/src/main/conf/hadoop-env.sh
@@ -75,3 +75,5 @@ export HADOOP_SECURE_DN_PID_DIR=${HADOOP_PID_DIR}
 
 # A string representing this instance of hadoop. $USER by default.
 export HADOOP_IDENT_STRING=$USER
+export HADOOP_ROOT_LOGGER=DEBUG,console
+
