b0bbee147f76085dcfa639eda2b8603e2e297c64
Lixiang Ao <liao@eng.ucsd.edu>
2017-06-15 09:15:39 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-cd hadoop-hdfs-project/hadoop-hdfs/src/main/specpaxos
-make finalLib -j4
-cd -
-mvn package -Pdist -DskipTests -Dmaven.javadoc.skip=true -Dtar
+cd hadoop-hdfs-project/hadoop-hdfs/src/main/specpaxos && make finalLib -j4 && cd - && mvn package -Pdist -DskipTests -Dmaven.javadoc.skip=true -Dtar
