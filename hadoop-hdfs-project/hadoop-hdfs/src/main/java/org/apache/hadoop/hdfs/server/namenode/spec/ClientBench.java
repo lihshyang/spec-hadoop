534b65df4c3e4667519d40256641c28db1a24a24
Lixiang Ao <aolixiang@gmail.com>
2017-06-16 17:46:25 -0700

    bench

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/spec/ClientBench.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/spec/ClientBench.java
@@ -13,55 +13,77 @@ public class ClientBench {
   static ClientProtocol service;
 
   public static void main(String[] args) throws IOException {
-    if (args.length < 2) {
-      System.err.println("Usage: ClientBench ha|spec ls|mkdir|rmdir [prefix]");
+    if (args.length < 4) {
+      System.err.println("Usage: ClientBench ha|spec ls|mkdir|rmdir NOPS SLEEPTIME [prefix]");
       return;
     }
 
     if (args[0].equals("ha")) {
       service = new HAClient(new HdfsConfiguration());
+    } else if (args[0].equals("spec")) {
+      service = new SpecClient();
     } else {
-      System.err.println("Usage: ClientBench ha|spec ls|mkdir|rmdir [prefix]");
+      System.err.println("Usage: ClientBench ha|spec ls|mkdir|rmdir NOPS SLEEPTIME [prefix]");
       return;
     }
 
-    if (args[1].equals("ls")) {
-      testLs();
-    } else if (args[1].equals("mkdir") && args.length == 3) {
-      testMkdir(args[2]);
-    } else if (args[1].equals("rmdir") && args.length == 3) {
-      testRmdir(args[2]);
+    if (args[1].equals("ls") && args.length == 4) {
+      testLs(args[2], args[3]);
+    } else if (args[1].equals("mkdir") && args.length == 5) {
+      testMkdir(args[2], args[3], args[4]);
+    } else if (args[1].equals("rmdir") && args.length == 5) {
+      testRmdir(args[2], args[3], args[4]);
     } else {
-      System.err.println("Usage: ClientBench ha|spec ls|mkdir|rmdir [prefix]");
+      System.err.println("Usage: ClientBench ha|spec ls|mkdir|rmdir NOPS SLEEPTIME [prefix]");
     }
   }
 
-  public static void testLs() throws IOException {
-    long last = System.nanoTime();
-    for (int i = 0; i < 10000; i++) {
+  public static void testLs(String nops, String sleepTime) throws IOException {
+    for (int i = 0; i < Integer.parseInt(nops); i++) {
+      long before = System.nanoTime();
       service.getListing("/", new byte[0], false);
-      System.out.println((System.nanoTime() - last) / 1000);
-      last = System.nanoTime();
+      long after = System.nanoTime();
+      System.out.println((after - before) / 1000);
+      try {
+        long t = Long.parseLong(sleepTime);
+        if (t > 0)
+          Thread.sleep(t);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
   }
 
-  public static void testMkdir(String prefix) throws IOException {
+  public static void testMkdir(String nops, String sleepTime, String prefix) throws IOException {
     service.mkdirs("/" + prefix, FsPermission.getDefault(), false);
-
-    long last = System.nanoTime();
-    for (int i = 10000; i < 20000; i++) {
+    for (int i = 10000; i < 10000 + Integer.parseInt(nops); i++) {
+      long before = System.nanoTime();
       service.mkdirs("/" + prefix + "/" + i, FsPermission.getDefault(), false);
-      System.out.println((System.nanoTime() - last) / 1000);
-      last = System.nanoTime();
+      long after = System.nanoTime();
+      System.out.println((after - before) / 1000);
+      try {
+        long t = Long.parseLong(sleepTime);
+        if (t > 0)
+          Thread.sleep(t);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
   }
 
-  public static void testRmdir(String prefix) throws IOException {
-    long last = System.nanoTime();
-    for (int i = 10000; i < 20000; i++) {
+  public static void testRmdir(String nops, String sleepTime, String prefix) throws IOException {
+    for (int i = 10000; i < 10000 + Integer.parseInt(nops); i++) {
+      long before = System.nanoTime();
       service.delete("/" + prefix + "/" + i, false);
-      System.out.println((System.nanoTime() - last) / 1000);
-      last = System.nanoTime();
+      long after = System.nanoTime();
+      System.out.println((after - before) / 1000);
+      try {
+        long t = Long.parseLong(sleepTime);
+        if (t > 0)
+          Thread.sleep(t);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
